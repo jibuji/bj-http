c48834aab8fb1b5d5488d43407aab5ebaa154467
mabuji <sunnypengfei@gmail.com>
2016-05-02 11:38:42 +0800

    add cookieRedirectOption

--- example.js
+++ example.js
@@ -1,27 +1,35 @@
-import {request} from './src';
+import {request, cookieRedirectOption} from './src';
+import tough from 'tough-cookie';
 
+let CookieJar = tough.CookieJar;
+let cookiesJar = new CookieJar();
 //https://list.tmall.com/search_product.htm?q=good&type=p
 let options = {
   // hostname: 'list.tmall.com',
   // path: '/search_product.htm?q=good&type=p',
   url: 'https://list.tmall.com/search_product.htm?q=good&type=p',
   method: 'GET',
-  maxRedirects: 10,
+  maxRedirects: 5,
   headers: {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'User-Agent': ' Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.86 Safari/537.36',
     'Accept-Language': ' en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4'
   },
   onRedirect: (res, options) => {
-    let location = res.headers['location'];
-    let newOption = {...options, url:location};
-    // console.log("onRedirect to :", location);
-    return newOption;
+    try {
+      let newOption = cookieRedirectOption(cookiesJar, res, options);
+      let location = res.headers['location'];
+      newOption = {...newOption, url:location};
+      console.log("onRedirect to :", location);
+      return newOption;
+    } catch (e) {
+      console.log('onRedirect error happened. e=', e);
+    }
   }
 };
 
 request(options).then((res)=>{
-  console.log('res.headers=', res.headers, ';res.body=', res.body);
+  console.log('res.headers=', res.headers);
 });
 
 
---
+++ src/cookieRedirectOption.js
@@ -0,0 +1,29 @@
+import {Cookie} from 'tough-cookie';
+
+const cookieRedirectOption = (cookiesJar, res, options) => {
+  let location = res.headers['location'];
+  let headerCookie = res.headers['set-cookie'];
+  if (headerCookie) {
+    // console.log('set headerCookie=', headerCookie);
+    let cookies = null;
+    if (headerCookie instanceof Array) {
+      cookies = headerCookie.map(Cookie.parse);
+    } else {
+      cookies = [Cookie.parse(headerCookie)];
+    }
+    cookies.forEach((cookie) => {
+      cookiesJar.setCookieSync(cookie, options.url);
+    });
+    // console.log('setCookieSync success');
+  }
+  let cookieString = cookiesJar.getCookieStringSync(location);
+  if (cookieString) {
+    let newOption = {...options};
+    // console.log('cookieString=', cookieString);
+    newOption.headers = {...newOption.headers, Cookie:cookieString};
+    return newOption;
+  }
+  return options;
+};
+
+export default cookieRedirectOption;
